21c6253ff254b6d7a3419480aa6b726e71236d9b
Rayhan9966 <rayhancse162gmail.com>
2024-06-08 14:07:12 +0400

    load data

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const env =require("dotenv")
+env.config()
 const express= require('express');
 const app =express();
 const cors=require('cors');
@@ -9,10 +11,9 @@ app.use(express.json());
 
 //MONGO
 
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.ptxksnq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ptxksnq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -26,16 +27,39 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+
+    const biodataCollection=client.db("bdMatrimonyDB").collection("biodata");
+app.get('/biodata', async (req,res) =>{
+    const result = await biodataCollection.find().toArray();
+    res.send(result);
+})
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close(); 
   }
 }
 run().catch(console.dir);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
 
 app.get('/',(req,res)=>{
     res.send('Matrimonial is running')
