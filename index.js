77288cd688db2ddfe7bee630d7b4495c508389df
Rayhan9966 <rayhancse162gmail.com>
2024-06-09 13:07:20 +0400

    usersss

--- index.js
+++ index.js
@@ -37,6 +37,12 @@ async function run() {
     //user api 
 app.post('/users', async (req,res)=>{
   const user=req.body;
+  //unique email
+  const query = { email: user.email };
+  const existingUser = await userCollection.findOne(query);
+  if(existingUser){
+    return res.send({message: 'user already exist', insertedId : null})
+  }
   const result = await userCollection.insertOne(user);
   res.send(result);
 
